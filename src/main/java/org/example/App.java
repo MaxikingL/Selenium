34379c30ef4f6ee8bfddaccc49f1e1b7366487f0
Lema <maksymilian.lema@capgemini.com>
2023-11-17 21:55:01 +0100

    LocatingBy

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,48 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class App {
+
+
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\malema\\workSpace\\Selenium\\Selenium\\driver\\chromedriver.exe");
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationtesting.co.uk/");
+
+        driver.findElement(By.linkText("CALCULATOR (JS)")).click();
+        driver.findElement(By.id("result")).sendKeys("1234");
+        driver.findElement(By.linkText("Automation Testing Test Arena")).click();
+
+        driver.findElement(By.linkText("BUTTONS")).click();
+        driver.findElement(By.xpath("// button[@id='btn_three']")).click();
+
+//        driver.findElement(By.id("btn_two")).click();
+//        driver.findElement(By.cssSelector("#btn_one")).click();
+//        driver.findElement(By.linkText("CALCULATOR (JS)")).click();
+//        driver.findElement(By.linkText("BUTTONS")).click();
+
+
+
+
+
+
+
+//        driver.quit();
+
+//        if (text.contains("Automation Testing")){
+//            System.out.printf("text is correct");
+//        }else{
+//            System.out.printf("text is not correct");
+//        }
+
+
+
     }
 }
---
+++ src/test/java/org/example/basicTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class basicTest extends TestCase {
+
+
+
+}
